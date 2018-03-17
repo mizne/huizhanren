26cc32af2632408cb630c69d31947887f04e22d8
mizne <w20054319@126.com>
2018-03-18 00:46:36 +0800

    fix img detector

--- src/pages/customer/directives/img-detector.directive.ts
+++ src/pages/customer/directives/img-detector.directive.ts
@@ -10,6 +10,7 @@ export class ImgDetectorDirective {
     const self = this
     this.img = this.el.nativeElement
     this.img.onload = function(ev) {
+      console.log(`height: ${this.height}; width: ${this.width}`)
       if (this.height > this.width) {
         self.addTransform()
       } else {
@@ -21,22 +22,29 @@ export class ImgDetectorDirective {
   private addTransform() {
     if (this.img) {
       if (this.imgDetector === 'editable') {
-        this.rd.setStyle(
-          this.img,
-          'transform',
-          'scale(0.7) rotate(-90deg) translateX(61%)'
-        )
+        const transform = `scale(0.6) rotate(-90deg) translateX(${this.computePositionForEdit(
+          this.img.height
+        )})`
+        console.log(transform)
+        this.rd.setStyle(this.img, 'transform', transform)
+
+        //  height: 516 0.6 88%
+        // height: 481 0.6 -79%
+        // height: 469 0.6 75%
+        // height: 459 0.6 73%
       }
       if (this.imgDetector === 'detailable') {
-        this.rd.setStyle(
-          this.img,
-          'transform',
-          'scale(0.7) rotate(-90deg) translateX(-63%)'
-        )
-      }
+        const transform = `scale(0.6) rotate(-90deg) translateX(${this.computePositionForDetail(
+          this.img.height
+        )})`
+        console.log(transform)
+        this.rd.setStyle(this.img, 'transform', transform)
 
-      this.rd.setStyle(this.img, 'width', '310px')
-      this.rd.setStyle(this.img, 'height', '460px')
+        //  height: 559 0.6 -105%
+        // height: 521 0.6 -95%
+        // height: 508 0.6 -92%
+        // height: 497 0.6 -88%
+      }
     }
   }
 
@@ -49,5 +57,19 @@ export class ImgDetectorDirective {
         this.rd.setStyle(this.img, 'transform', 'none')
       }
     }
+  }
+
+  private computePositionForEdit(height: number): string {
+    const offsetHeight = 516 - height
+    const transalteX = 88
+    const res = transalteX - offsetHeight / 4
+    return String(res) + '%'
+  }
+
+  private computePositionForDetail(height: number): string {
+    const offsetHeight = 559 - height
+    const transalteX = 105
+    const res = transalteX - offsetHeight / 3.7
+    return String(-res) + '%'
   }
 }
